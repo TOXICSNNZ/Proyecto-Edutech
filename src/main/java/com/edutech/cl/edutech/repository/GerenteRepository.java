4fd60680599371a28a4c8ed06b67d92a386a4f26
TOXICSNNZ <sebastianpower21@gmail.com>
2025-07-01 18:13:06 -0400

    Delete src directory

--- src/main/java/com/edutech/cl/edutech/repository/GerenteRepository.java
+++
@@ -1,9 +0,0 @@
-package com.edutech.cl.edutech.repository;
-
-import com.edutech.cl.edutech.model.Gerente;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface GerenteRepository extends JpaRepository<Gerente, Integer> {
-}
